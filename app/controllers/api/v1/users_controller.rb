b1cbddefab6159f5deac82c5ae19289e3acb1c34
Franco Prudhomme <francoprud@gmail.com>
2016-03-25 15:27:26 -0300

    Changes the way of rendering responses in controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  respond_to :json
-
   def show
-    respond_with User.find(params[:id])
+    render json: User.find(params[:id])
   end
 end
