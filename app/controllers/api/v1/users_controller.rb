fc378ffa81d6e5253b7ba97ba2f1807ff246908d
Franco Prudhomme <francoprud@gmail.com>
2016-03-25 22:42:50 -0300

    Adds before_filter to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,30 +1,30 @@
 class Api::V1::UsersController < ApplicationController
+  before_filter :find_user, only: [:show, :update, :destroy]
+
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user, status: 201, location: [:api, user]
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 201, location: [:api, @user]
     else
-      render json: { errors: user.errors }, status: 422
+      render json: { errors: @user.errors }, status: 422
     end
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update(user_params)
-      render json: user, status: 200, location: [:api, user]
+    if @user.update(user_params)
+      render json: @user, status: 200, location: [:api, @user]
     else
-      render json: { errors: user.errors }, status: 422
+      render json: { errors: @user.errors }, status: 422
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-    if user.present?
-      user.destroy
+    if @user.present?
+      @user.destroy
     end
     head 204
   end
@@ -33,5 +33,9 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
