76549b4b9cac6baf654607037014bf0472015af0
Franco Prudhomme <francoprud@gmail.com>
2016-04-02 16:46:53 -0300

    Updates the total calculation for order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -16,14 +16,15 @@ describe Order do
   it { should have_many(:products).through(:placements) }
 
   describe '#set_total!' do
-    let!(:first_product)  { create(:product, price: 50.0) }
-    let!(:second_product) { create(:product, price: 100.0) }
-    let!(:products_list)  { [first_product, second_product] }
-    let!(:order)          { create(:order, product_ids: products_list.map(&:id)) }
-    let(:expected_total)  { products_list.map(&:price).sum }
+    let!(:first_product)    { create(:product, price: 50.0, quantity: 5) }
+    let!(:second_product)   { create(:product, price: 100.0, quantity: 4) }
+    let!(:first_placement)  { build(:placement, product: first_product, quantity: 1) }
+    let!(:second_placement) { build(:placement, product: second_product, quantity: 2) }
+    let!(:order)            { build(:order, placements: [first_placement, second_placement]) }
+    let(:expected_total)    { first_product.price * first_placement.quantity + second_product.price * second_placement.quantity }
 
     it 'returns the total amount to pay for the products' do
-      expect(order.total).to eq expected_total
+      expect(order.set_total!).to eq expected_total
     end
   end
 
